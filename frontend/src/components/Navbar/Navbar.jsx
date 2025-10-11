1c9bab037171d08b8992f69b1a8f781d920f7798
Alesja Delja <alesja.delja@studio.unibo.it>
2025-10-11 23:53:14 +0200

    Added the navbar component

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import {Link} from "react-router-dom";
+import ".Navbar.css";
+
+export default function Navbar(){
+    const links = [
+        "Home",
+        "Travel Plans",
+        "Financial Balance",
+        "Books",
+        "TV Series",
+        "Manhwa",
+        "Shopping List",
+        "Games",
+        "Sites",
+        "Anime"
+    ];
+
+    return(
+        <nav>
+            <ul>
+                {links.map((text) =>(
+                    <li key={text}>
+                        <Link to={`/${text.toLowerCase().replace(/\s+/g, "-")}`}>{text}</Link>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    );
+}
