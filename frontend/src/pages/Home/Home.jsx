06d6561147112b9989e922a86e663263bb46305a
Alesja Delja <alesja.delja@studio.unibo.it>
2025-10-12 17:30:33 +0200

    Added the main in home and its style

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -4,6 +4,11 @@ export default function Home() {
   return (
     <div className="container">
       <Header />
+      <main>
+        <div className="content">
+          
+        </div>
+      </main>
       {/**
       Qui c'è anche il main con il calendario e tutto
        */}
