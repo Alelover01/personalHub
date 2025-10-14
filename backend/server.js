390dc0472f0c994824cc043d9bad0c68f9b60a33
Alesja Delja <alesja.delja@studio.unibo.it>
2025-10-14 22:27:41 +0200

    Fix wildcard route for Express v5

--- backend/server.js
+++ backend/server.js
@@ -53,7 +53,7 @@ app.delete('/events/:id', (req, res) => {
 // ---- SERVE IL FRONTEND REACT ----
 app.use(express.static(path.join(__dirname, '..', 'frontend', 'build')));
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'frontend', 'build', 'index.html'));
 });
 
