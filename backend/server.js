951a3af8b935e9b364b90c217440efa152a4d56a
Alesja Delja <alesja.delja@studio.unibo.it>
2025-10-18 17:30:17 +0200

    Fixed the dirname

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,16 @@
 import express from 'express';
-import path from 'path';
 import fetch from 'node-fetch';
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 const BIN_URL = process.env.REACT_APP_JSONBIN_URL;
 const API_KEY = process.env.REACT_APP_JSONBIN_API_KEY;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(express.json());
 // Serve i file statici React 
