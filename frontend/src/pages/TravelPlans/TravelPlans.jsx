48ce9927484bc01befa16ca66b6bc64ccd6d430f
Alesja Delja <alesja.delja@studio.unibo.it>
2025-10-18 23:39:33 +0200

    Added of leaflet

--- frontend/src/pages/TravelPlans/TravelPlans.jsx
+++ frontend/src/pages/TravelPlans/TravelPlans.jsx
@@ -1,8 +1,10 @@
+import WorldMap from "../../components/WorldMap/WorldMap";
+import 'leaflet/dist/leaflet.css';
 export default function TravelPlans() {
   return (
     <div>
       <h1>Travel Plans</h1>
-      <p>Qui puoi pianificare i tuoi viaggi!</p>
+      <WorldMap />
     </div>
   );
 }
