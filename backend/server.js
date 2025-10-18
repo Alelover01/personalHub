ddff11ed854c216b3b99da41d897df547c483de4
Alesja Delja <alesja.delja@studio.unibo.it>
2025-10-18 18:13:39 +0200

    Another try 3.2

--- backend/server.js
+++ backend/server.js
@@ -15,9 +15,7 @@ const __dirname = path.dirname(__filename);
 app.use(express.json());
 // Serve i file statici React 
 app.use(express.static(path.join(__dirname, '../frontend/build')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
-});
+
 // 🔹 Legge i post-it dal bin privato
 app.get('/postits', async (req, res) => {
   try {
@@ -74,6 +72,9 @@ app.post('/postits', async (req, res) => {
   }
 });
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+});
 app.listen(PORT, () => {
   console.log(`🚀 Server avviato su http://localhost:${PORT}`);
 });
