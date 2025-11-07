d197f3f4528a1c73fb1ed931e15558ea9ee21323
Alesja Delja <alesja.delja@studio.unibo.it>
2025-11-07 21:55:44 +0100

    Fix of the ipv4

--- backend/db.js
+++ backend/db.js
@@ -1,19 +1,11 @@
 import postgres from "postgres";
 import dotenv from "dotenv";
-import dns from "dns/promises";
 
 dotenv.config();
 
-const connectionString = process.env.DATABASE_URL;
+const connectionString = process.env.DATABASE_URL + "?sslmode=require";
 
-const { address } = await dns.lookup("db.juhfnvmfoiikdnoizors.supabase.co", { family: 4 });
-
-const sql = postgres({
-  host: address,
-  port: 5432,
-  user: "postgres",
-  password: "GubUW7Z03c9HWcf2",
-  database: "postgres",
+const sql = postgres(connectionString, {
   ssl: "require",
 });
 
