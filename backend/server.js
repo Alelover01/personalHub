ad0fe7cc124d390ed7c21b2ea60b18ce2ffd4df4
Alesja Delja <alesja.delja@studio.unibo.it>
2025-11-11 22:44:33 +0100

    Testing the database 2.0

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const POSTIT_BIN_URL = process.env.JSONBIN_URL;         // bin post-it
 const EVENT_BIN_URL = process.env.JSONBIN_URL_EVENTS;  // bin eventi
-const API_KEY = process.env.JSONBIN_API_KEY;           
+const API_KEY = process.env.JSONBIN_API_KEY;
 const JWT_SECRET = process.env.JWT_SECRET;
 const upload = multer({ dest: 'uploads/' });
 
@@ -44,16 +44,16 @@ app.use((req, res, next) => {
 });
 /* ================= AUTH ROUTES ================= */
 //Rotta di registrazione
-app.post('/auth/register',upload.single('profilePicture'), async(req,res)=>{
-  const {username, email, password } = req.body;
+app.post('/auth/register', upload.single('profilePicture'), async (req, res) => {
+  const { username, email, password } = req.body;
   const saltRounds = 10;
-  if (!username || !email || !password){
-    return res.status(400).json({message: 'Tutti i campi sono obbligatori.'});
+  if (!username || !email || !password) {
+    return res.status(400).json({ message: 'Tutti i campi sono obbligatori.' });
   }
-  try{
+  try {
     //Se caricato il file lo invio a Cloudinary
     let profilePictureUrl = null;
-    if (req.file){
+    if (req.file) {
       const uploadResult = await cloudinary.uploader.upload(req.file.path, {
         folder: 'profile_picture',
         public_id: username,
@@ -63,68 +63,73 @@ app.post('/auth/register',upload.single('profilePicture'), async(req,res)=>{
     }
     //Hash della password
     const hashedPassword = await bcrypt.hash(password, saltRounds);
-    const result = await sql `
+    const result = await sql`
             INSERT INTO profiles (username, email, password, profile_picture)
             VALUES (${username}, ${email}, ${hashedPassword}, ${profilePictureUrl})
             RETURNING id, username, email, profile_picture;
         `;
-        const token = jwt.sign({id: result[0].id, username: result[0].username}, JWT_SECRET, {expiresIn: '1d'});
+    const token = jwt.sign({ id: result[0].id, username: result[0].username }, JWT_SECRET, { expiresIn: '1d' });
 
-        res.status(201).json({
-          message: 'Registrazione completata con successo!',
-          user: { id: result[0].id, username: result[0].username, email: result[0].email, profilePicture: result[0].profilePicture},
-          token
-        });
-  } catch (err){
+    res.status(201).json({
+      message: 'Registrazione completata con successo!',
+      user: { id: result[0].id, username: result[0].username, email: result[0].email, profilePicture: result[0].profilePicture },
+      token
+    });
+  } catch (err) {
     console.error('Errore di registrazione:', err);
-    //Gestione errore di violazione dei constraint UNICI
-    if(err.code === '23505'){
+
+    if (err.code === '23505') {
       let field = 'campo';
       if (err.detail.includes('email')) field = 'Email';
       else if (err.detail.includes('username')) field = 'Username';
-      return res.status(409).json({message: `${field} già in uso.`});
+      return res.status(409).json({ message: `${field} già in uso.` });
     }
-    res.status(500).json({message: 'Errore interno del server durante la registrazione.' , error: err.message});
+
+    return res.status(500).json({
+      message: 'Errore interno del server durante la registrazione.',
+      error: err.message
+    });
   }
+
 });
 //Rotta di Login
 app.post('/auth/login', async (req, res) => {
-    const { username, password } = req.body;
-    
-    if (!username || !password) {
-        return res.status(400).json({ message: 'Username e Password sono obbligatori.' });
-    }
+  const { username, password } = req.body;
 
-    try {
-        const users = await sql`
+  if (!username || !password) {
+    return res.status(400).json({ message: 'Username e Password sono obbligatori.' });
+  }
+
+  try {
+    const users = await sql`
             SELECT id, username, password 
             FROM profiles 
             WHERE username = ${username} OR email = ${username};
         `;
 
-        const user = users[0];
+    const user = users[0];
 
-        if (!user) {
-            return res.status(401).json({ message: 'Credenziali non valide.' });
-        }
-        const isMatch = await bcrypt.compare(password, user.password);
-
-        if (!isMatch) {
-            return res.status(401).json({ message: 'Credenziali non valide.' });
-        }
-
-        const token = jwt.sign({ id: user.id, username: user.username }, JWT_SECRET, { expiresIn: '1d' });
-
-        res.json({ 
-            message: 'Login completato con successo!',
-            user: { id: user.id, username: user.username },
-            token
-        });
-
-    } catch (err) {
-        console.error("Errore di Login:", err);
-        res.status(500).json({ message: 'Errore interno del server durante il login.' });
+    if (!user) {
+      return res.status(401).json({ message: 'Credenziali non valide.' });
     }
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(401).json({ message: 'Credenziali non valide.' });
+    }
+
+    const token = jwt.sign({ id: user.id, username: user.username }, JWT_SECRET, { expiresIn: '1d' });
+
+    res.json({
+      message: 'Login completato con successo!',
+      user: { id: user.id, username: user.username },
+      token
+    });
+
+  } catch (err) {
+    console.error("Errore di Login:", err);
+    res.status(500).json({ message: 'Errore interno del server durante il login.' });
+  }
 });
 /* ================= POST-IT ROUTES ================= */
 
