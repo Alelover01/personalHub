aba8694f148bafbc24a7e1319c9653b3c9601d21
Alesja Delja <alesja.delja@studio.unibo.it>
2025-11-11 22:08:30 +0100

    Saved the image

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import sql from './db.js';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import multer from 'multer';
+import { v2 as cloudinary } from 'cloudinary';
 
 dotenv.config();
 
@@ -23,6 +24,13 @@ const __dirname = path.dirname(__filename);
 
 app.use(express.json());
 
+//Configurazione dei cloudinary
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
 // Serve i file statici React
 app.use(express.static(path.join(__dirname, '../frontend/build')));
 
@@ -36,26 +44,34 @@ app.use((req, res, next) => {
 /* ================= AUTH ROUTES ================= */
 //Rotta di registrazione
 app.post('/auth/register',upload.single('profilePicture'), async(req,res)=>{
-  console.log("BODY:", req.body);
-  console.log("FILE:", req.file);
   const {username, email, password } = req.body;
   const saltRounds = 10;
   if (!username || !email || !password){
     return res.status(400).json({message: 'Tutti i campi sono obbligatori.'});
   }
   try{
+    //Se caricato il file lo invio a Cloudinary
+    let profilePictureUrl = null;
+    if (req.file){
+      const uploadResult = await cloudinary.uploader.upload(req.file.path, {
+        folder: 'profilePicture',
+        public_id: username,
+        resource_type: 'image'
+      });
+      profilePictureUrl = uploadResult.secure_url;
+    }
     //Hash della password
     const hashedPassword = await bcrypt.hash(password, saltRounds);
     const result = await sql `
-            INSERT INTO profiles (username, email, password)
-            VALUES (${username}, ${email}, ${hashedPassword})
-            RETURNING id, username, email;
+            INSERT INTO profiles (username, email, password, profilePicture)
+            VALUES (${username}, ${email}, ${hashedPassword}, ${profilePictureUrl})
+            RETURNING id, username, email, profilePicture;
         `;
         const token = jwt.sign({id: result[0].id, username: result[0].username}, JWT_SECRET, {expiresIn: '1d'});
 
         res.status(201).json({
           message: 'Registrazione completata con successo!',
-          user: { id: result[0].id, username: result[0].username, email: result[0].email },
+          user: { id: result[0].id, username: result[0].username, email: result[0].email, profilePicture: result[0].profilePicture},
           token
         });
   } catch (err){
