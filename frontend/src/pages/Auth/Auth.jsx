7bd02400a144bfcd659beca7e0725d48e38eafb4
Alesja Delja <alesja.delja@studio.unibo.it>
2025-11-13 16:49:07 +0100

    Changed the style of login/sign in

--- frontend/src/pages/Auth/Auth.jsx
+++ frontend/src/pages/Auth/Auth.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Auth.css"; 
-
+import '../../styles/global.css';
 /**
  * Auth Component
  * Handles both Login and Registration flows with form validation,
